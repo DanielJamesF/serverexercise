6ee193b267a080b22860569530b58014060034ac
DanielJamesF <fredericksd17@gmail.com>
2022-07-27 13:54:10 +0200

    first commit

---
+++ courses.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+let courses = [{
+    id: 1,
+    name: 'Software Engineering'
+    },
+    {
+        id: 2,
+        name: 'Web Development'
+    },
+    {
+        id: 3,
+        name: 'Database Management'
+    },
+    {
+        id: 4,
+        name: 'Duck Duck Goose'
+    }
+];
+
+app.get('/', (req, res) => {
+    res.json(courses);
+});
+
+app.get('/:id', (req, res) => {
+    if(req.params.id >  courses.length){
+        res.send(
+            `<h1>There is no record with this id</h1>`
+        )
+    }else{
+        res.send(JSON.stringify(courses[req.params.id - 1]))
+    }
+});
